f654dbbcdd5cd5d11d5483500bb03bf323510002
OscarGil03 <oscargil0301@gmail.com>
2022-12-13 23:40:20 -0700

    estilo en las tostadas locas jajaja

--- App/Screens/TostadasScreen.js
+++ App/Screens/TostadasScreen.js
@@ -1,36 +1,81 @@
 import { Text, View, TouchableOpacity, Image } from "react-native";
-import { Divider } from 'react-native-paper';
+import { Divider } from "react-native-paper";
 import React from "react";
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import { ScrollView } from "react-native-gesture-handler";
 import styles from "../estilos";
+import { Feather } from "@expo/vector-icons";
 
 const Tostadas = ({ navigation }) => {
-    return (
-        <ScrollView>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Image
-                    //Imagen Tostada
-                    source={{
-                        uri: 'https://i.ibb.co/n00bmmF/image.png',
-                    }}
-                    style={{ width: 250, height: 150, borderRadius: 10, margin: 15 }}
-                />
-                <Text style={styles.textote}>Tostadas "El Brayan"</Text>
-                <Text style={styles.descripcion}>Estan tremendas!!!</Text>
+  return (
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Image
+          //Imagen Tostada
+          source={{
+            uri: "https://i.ibb.co/n00bmmF/image.png",
+          }}
+          style={styles.img_select}
+        />
+        <Text style={styles.textote}>Tostadas "El Brayan"</Text>
+        <Text style={[styles.descripcion, { fontSize: 16 }]}>
+          Estan tremendas!!!
+        </Text>
+      </View>
+      <Divider style={{ margin: 10 }} />
+      <View style={{ flex: 1 }}>
+        <Text style={styles.textotent}>Cupones disponibles</Text>
+        <View
+          style={[
+            styles.selec_c,
+            {
+              borderRadius: 10,
+            },
+          ]}
+        >
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("Cupon", {
+                Desc_C: "Obten 35% de descuento en una orden de tostadas",
+                Cad_C: "Valido hasta el 15/04/2023 a las 13:00 horas",
+              })
+            }
+          >
+            <Text style={styles.descripcion2}>
+              Obten 35% de descuento en una orden de tostadas
+            </Text>
+            <View style={styles.flechita_select}>
+              <Feather name="arrow-right" size={24} color="black" />
             </View>
-            <Divider style={{ margin: 10, }} />
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text style={styles.textote}>Cupones disponibles</Text>
-                <TouchableOpacity onPress={() => navigation.navigate('Cupon', { Desc_C: "Obten 35% de descuento en una orden de tostadas", Cad_C: "Valido hasta el 15/04/2023 a las 13:00 horas" })}>
-                    <Text style={styles.selec_c}>Obten 35% de descuento en una orden de tostadas</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Cupon', { Desc_C: "Obten una bebida gratis en la compra de 2 tostadas", Cad_C: "Valido hasta el 15/04/2023 a las 15:00 horas" })}>
-                    <Text style={styles.selec_c}>Obten una bebida gratis en la compra de 2 tostadas</Text>
-                </TouchableOpacity>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={[
+            styles.selec_c,
+            {
+              borderRadius: 10,
+            },
+          ]}
+        >
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("Cupon", {
+                Desc_C: "Obten una bebida gratis en la compra de 2 tostadas",
+                Cad_C: "Valido hasta el 15/04/2023 a las 15:00 horas",
+              })
+            }
+          >
+            <Text style={styles.descripcion2}>
+              Obten una bebida gratis en la compra de 2 tostadas
+            </Text>
+            <View style={styles.flechita_select}>
+              <Feather name="arrow-right" size={24} color="black" />
             </View>
-        </ScrollView>
-    );
-}
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
 
 export default Tostadas;
